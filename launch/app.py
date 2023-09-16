3a956294e03a54d29a7310feeae0f96dad1a2d09
Brett <bdytx5@umsystem.edu>
2023-09-16 09:56:37 -0500

    added context max len arg

--- launch/app.py
+++ launch/app.py
@@ -5,6 +5,7 @@ import sys
 from collections import deque
 
 # Check for --test and --port in the command line arguments
+##### NOTE --test --port 4000 as args with the tst_chat_backend.py script running will launch the app in test mode 
 is_test = '--test' in sys.argv
 port_index = sys.argv.index('--port') if '--port' in sys.argv else None
 port = int(sys.argv[port_index + 1]) if port_index else 5000
@@ -12,11 +13,17 @@ port = int(sys.argv[port_index + 1]) if port_index else 5000
 # Decide the backend URL
 backend_url = "http://localhost:8080/predict" if not is_test else f"http://127.0.0.1:{port}/predict"
 
-# Cache to hold previous chat messages and responses
-chat_cache = deque(maxlen=100)
-
 # Number of tokens per word
 tokens_per_word = 2
+
+# Number of previous tokens to use as context, default 250
+num_prev_tokens = 250
+
+# Cache max length as an argument
+cache_maxlen = int(sys.argv[sys.argv.index('--contextlen') + 1]) if '--contextlen' in sys.argv else 100
+
+# Cache to hold previous chat messages and responses
+chat_cache = deque(maxlen=cache_maxlen)
 
 app = Flask(__name__)
 
@@ -37,26 +44,28 @@ def send_message():
     data = request.json
     new_prompt = f"User: {data['prompt']}\n"
     
+    # Update the chat cache
     chat_cache.append(new_prompt)
 
+    # Count tokens based on custom token rule
     tokens = sum(len(word.split()) for sentence in chat_cache for word in sentence.split()) * tokens_per_word
 
+    # Concatenate all chat history to form a single prompt
     full_prompt = ''.join(chat_cache)
 
-    payload = {'prompt': full_prompt}
+    # Create payload with concatenated prompt
+    payload = {'prompt': full_prompt, 'previous_tokens': num_prev_tokens}
 
     try:
         response = requests.post(backend_url, json=payload)
         api_response = f"Bot: {response.json()['response']}\n"
         
+        # Update chat cache
         chat_cache.append(api_response)
 
         return api_response.strip()
     except Exception as e:
         return str(e)
-
-def run_server():
-    app.run(host='0.0.0.0', port=5000)
 
 html_content = """
 <!DOCTYPE html>
@@ -75,7 +84,6 @@ html_content = """
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ 'prompt': message })
                 }).then(response => response.text()).then(data => {
-                    data = data.replace(/<\/?s>/g, '');
                     document.getElementById('loader').style.display = 'none';
                     document.getElementById('chat').innerHTML += '<p><strong>API:</strong> ' + data + '</p>';
                     document.getElementById('userInput').value = '';
@@ -103,6 +111,6 @@ html_content = """
 """
 
 if __name__ == "__main__":
-    t = threading.Thread(target=run_server)
+    t = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': port})
     t.start()
-    print(f"Visit http://127.0.0.1:{5000}/chat to start chatting.")
+    print(f"Visit http://127.0.0.1:{port}/chat to start chatting.")
