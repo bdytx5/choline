3128b07956b60c77973f87ec24d8d9f6405ca1f0
Brett <bdytx5@umsystem.edu>
2023-09-16 09:51:24 -0500

    added context and clear chat in chat UI

--- launch/app.py
+++ launch/app.py
@@ -1,145 +1,8 @@
-# import subprocess
-# import threading
-# import requests
-# from flask import Flask, request, jsonify, render_template_string
-
-# app = Flask(__name__)
-
-# @app.route('/chat')
-# def chat():
-#     return render_template_string(html_content)
-
-# @app.route('/send_message', methods=['POST'])
-# def send_message():
-#     data = request.json
-#     prompt = data['prompt']
-#     payload = {
-#     'prompt': prompt,
-    
-#     }
-
-    
-#     try:
-#         response = requests.post("http://localhost:8080/predict", json=payload)
-        
-#         api_response = response.text
-#         return api_response
-#     except Exception as e:
-#         return str(e)
-
-# def run_server():
-#     app.run(host='0.0.0.0', port=5000)
-
-# # HTML content and threading
-# html_content = """
-# <!DOCTYPE html>
-# <html>
-# <head>
-#     <title>Chat with API</title>
-#     <script>
-#         async function sendMessage() {
-#             const message = document.getElementById("userInput").value;
-#             document.getElementById("chat").innerHTML += "<p><strong>You:</strong> " + message + "</p>";
-#             document.getElementById("loader").style.display = "block";
-            
-#             const response = await fetch('/send_message', {
-#                 method: 'POST',
-#                 headers: { 'Content-Type': 'application/json' },
-#                 body: JSON.stringify({ "prompt": message })
-#             });
-
-#             let data = await response.text();
-#             data = data.replace(/<\/?s>/g, '');  // Remove strikethrough tags
-#             document.getElementById("loader").style.display = "none";
-#             document.getElementById("chat").innerHTML += "<p><strong>API:</strong> " + data + "</p>";
-#         }
-#     </script>
-# </head>
-# <body>
-#     <div id="loader" style="display:none;">Loading...</div>
-#     <div id="chat"></div>
-#     <input id="userInput" type="text"/>
-#     <button onclick="sendMessage()">Send</button>
-# </body>
-# </html>
-# """
-
-# if __name__ == "__main__":
-#     t = threading.Thread(target=run_server)
-#     t.start()
-#     print("Visit http://127.0.0.1:5000/chat to start chatting.")
-
-# import subprocess
-# import threading
-# import requests
-# from flask import Flask, request, jsonify, render_template_string
-
-# app = Flask(__name__)
-
-# @app.route('/chat')
-# def chat():
-#     return render_template_string(html_content)
-
-# @app.route('/send_message', methods=['POST'])
-# def send_message():
-#     data = request.json
-#     prompt = data['prompt']
-#     payload = {'prompt': prompt}
-    
-#     try:
-#         response = requests.post("http://localhost:8080/predict", json=payload)
-#         api_response = response.text
-#         return api_response
-#     except Exception as e:
-#         return str(e)
-
-# def run_server():
-#     app.run(host='0.0.0.0', port=5000)
-
-# html_content = """
-# <!DOCTYPE html>
-# <html>
-# <head>
-#     <title>Chat with API</title>
-#     <script>
-#         function sendMessage(event) {
-#             if (event.keyCode === 13) {
-#                 const message = document.getElementById("userInput").value;
-#                 document.getElementById("chat").innerHTML += "<p><strong>You:</strong> " + message + "</p>";
-#                 document.getElementById("loader").style.display = "block";
-
-#                 fetch('/send_message', {
-#                     method: 'POST',
-#                     headers: { 'Content-Type': 'application/json' },
-#                     body: JSON.stringify({ "prompt": message })
-#                 }).then(response => response.text()).then(data => {
-#                     data = data.replace(/<\/?s>/g, '');  // Remove strikethrough tags
-#                     document.getElementById("loader").style.display = "none";
-#                     document.getElementById("chat").innerHTML += "<p><strong>API:</strong> " + data + "</p>";
-#                     document.getElementById("userInput").value = '';
-#                 });
-#             }
-#         }
-#     </script>
-# </head>
-# <body>
-#     <div id="loader" style="display:none;">Loading...</div>
-#     <div id="chat"></div>
-#     <textarea id="userInput" style="width: 100%; height: 50px; resize: both;" onkeydown="sendMessage(event)"></textarea>
-# </body>
-# </html>
-# """
-
-# if __name__ == "__main__":
-#     t = threading.Thread(target=run_server)
-#     t.start()
-#     print("Visit http://127.0.0.1:5000/chat to start chatting.")
-
-import subprocess
-import threading
+from flask import Flask, request, render_template_string
 import requests
+import threading
 import sys
-from flask import Flask, request, jsonify, render_template_string
+from collections import deque
 
 # Check for --test and --port in the command line arguments
 is_test = '--test' in sys.argv
@@ -149,22 +12,46 @@ port = int(sys.argv[port_index + 1]) if port_index else 5000
 # Decide the backend URL
 backend_url = "http://localhost:8080/predict" if not is_test else f"http://127.0.0.1:{port}/predict"
 
+# Cache to hold previous chat messages and responses
+chat_cache = deque(maxlen=100)
+
+# Number of tokens per word
+tokens_per_word = 2
+
 app = Flask(__name__)
 
 @app.route('/chat')
 def chat():
     return render_template_string(html_content)
 
+@app.route('/clear_chat', methods=['POST'])
+def clear_chat():
+    global chat_cache
+    chat_cache.clear()
+    return 'Cleared'
+
 @app.route('/send_message', methods=['POST'])
 def send_message():
+    global chat_cache
+    
     data = request.json
-    prompt = data['prompt']
-    payload = {'prompt': prompt}
+    new_prompt = f"User: {data['prompt']}\n"
+    
+    chat_cache.append(new_prompt)
+
+    tokens = sum(len(word.split()) for sentence in chat_cache for word in sentence.split()) * tokens_per_word
+
+    full_prompt = ''.join(chat_cache)
+
+    payload = {'prompt': full_prompt}
 
     try:
         response = requests.post(backend_url, json=payload)
-        api_response = response.text
-        return api_response
+        api_response = f"Bot: {response.json()['response']}\n"
+        
+        chat_cache.append(api_response)
+
+        return api_response.strip()
     except Exception as e:
         return str(e)
 
@@ -178,7 +65,7 @@ html_content = """
     <title>Chat with API</title>
     <script>
         function sendMessage(event) {
-            if (event.keyCode === 13 && event.shiftKey) {
+            if (event.shiftKey && event.keyCode === 13) {
                 const message = document.getElementById('userInput').value;
                 document.getElementById('chat').innerHTML += '<p><strong>You:</strong> ' + message + '</p>';
                 document.getElementById('loader').style.display = 'block';
@@ -188,20 +75,29 @@ html_content = """
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ 'prompt': message })
                 }).then(response => response.text()).then(data => {
-                    data = data.replace(/<\/?s>/g, '');  // Remove strikethrough tags
+                    data = data.replace(/<\/?s>/g, '');
                     document.getElementById('loader').style.display = 'none';
                     document.getElementById('chat').innerHTML += '<p><strong>API:</strong> ' + data + '</p>';
                     document.getElementById('userInput').value = '';
                 });
             }
         }
+
+        function clearChat() {
+            fetch('/clear_chat', {
+                method: 'POST'
+            }).then(() => {
+                document.getElementById('chat').innerHTML = '';
+            });
+        }
     </script>
 </head>
 <body>
     <div id='loader' style='display:none;'>Loading...</div>
-    <div id='chat'></div>
+    <div id='chat' style='padding-bottom: 50px;'></div>
     <textarea id='userInput' style='width: 100%; height: 50px; resize: both;' onkeydown='sendMessage(event)'></textarea>
-    <p>Helper: To get a response, hit Shift+Enter</p>
+    <p>To get a response, hit Shift+Enter.</p>
+    <button onclick='clearChat()'>Clear Chat</button>
 </body>
 </html>
 """
--- launch/tst/tst_chat_backend.py
+++ launch/tst/tst_chat_backend.py
@@ -7,7 +7,7 @@ def predict():
     data = request.json
     prompt = data.get('prompt', '')
     reversed_prompt = prompt[::-1]
-    return jsonify({"response": reversed_prompt})
+    return jsonify({"response": prompt + "!!!tst resonse!!"})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=4000)
