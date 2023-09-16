641315fbb40c99ad26ed64b5d752ff4a66f6f587
Brett <bdytx5@umsystem.edu>
2023-09-16 12:47:43 -0500

    token increase 8k

--- launch/launch.py
+++ launch/launch.py
@@ -46,7 +46,7 @@ def predict():
     # Generate the response
     output = model.generate(
         inputs["input_ids"],
-        max_length=250,
+        max_length=8000,
         do_sample=True,
         top_p=0.9,
         temperature=0.1
